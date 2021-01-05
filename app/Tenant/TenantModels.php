08aa5cc1bcecd1d0d3a142e2d6ea3774fc4a9a92
Max Rodrigues <maxuel.rodrigues@gmail.com>
2021-01-05 17:09:03 -0300

    solving creating data

--- app/Tenant/TenantModels.php
+++ app/Tenant/TenantModels.php
@@ -2,11 +2,19 @@
 
 namespace App\Tenant;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
 trait TenantModels
 {
     protected static function boot()
     {
         parent::boot();
         static::addGlobalScope(new TenantScope());
+
+        static::creating(function (Model $model) {
+            $companyId = Auth::user()->company_id;
+            $model->company_id = $companyId;
+        });
     }
 }
